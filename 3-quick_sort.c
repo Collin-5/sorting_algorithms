9b7670afdefc869e3ce964d7ff1582ab94cfc432
Collin-5 <collins380@gmail.com>
2021-10-26 10:35:54 +0300

    correcting error with betty

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,15 +7,17 @@
  * @size: full size of array
  * Return: pvt position
  */
-int partition(int *array, int start, int end, int size)
+int partition(int *array, int lo, int hi, size_t size)
 {
-	int pvt = array[end];
-	int i = start, j, tmp;
+	int i, j, pivot, tmp;
 
-	for (j = start; j < end; j++)
+	pivot = array[hi];
+	i = lo - 1;
+	for (j = lo; j < hi; j++)
 	{
-		if (array[j] <= pvt)
+		if (array[j] <= pivot)
 		{
+			i = i + 1;
 			if (i != j)
 			{
 				tmp = array[i];
@@ -23,47 +25,50 @@ int partition(int *array, int start, int end, int size)
 				array[j] = tmp;
 				print_array(array, size);
 			}
-			i++;
 		}
 	}
-	if (i != end)
+	if (i + 1 != hi)
 	{
-		tmp = array[i];
-		array[i] = array[end];
-		array[end] = tmp;
+		tmp = array[i + 1];
+		array[i + 1] = array[hi];
+		array[hi] = tmp;
 		print_array(array, size);
 	}
-	printf("return i=%d\n", i);
-	return (i);
-}
-/**
- * quickSort - quick sorts with recursion
- * @array: array to sort through
- * @start: start of array or subarray
- * @end: end of array or subarray
- * @size: size of full array
- */
-void quickSort(int *array, int start, int end, int size)
-{
-	int pvt;
 
-	if (start < end)
+	return (i + 1);
+}
+
+/**
+ * quicksort - implementation of the quick sort algorithm
+ * @array: the array to sort
+ * @lo: the lo
+ * @hi: the high
+ * @size: size of the array
+ *
+ * Return: void
+ */
+void quicksort(int *array, int lo, int hi, size_t size)
+{
+	int p;
+
+	if (lo < hi)
 	{
-		pvt = partition(array, start, end, size);
-		printf("first recursive, start [%d] to pivot-1[%d]\n", start, pvt - 1);
-		quickSort(array, start, pvt - 1, size);
-		printf("second recursive, pivot+1 [%d] to end [%d]\n", pvt + 1, end);
-		quickSort(array, pvt + 1, end, size);
+		p = partition(array, lo, hi, size);
+		quicksort(array, lo, p - 1, size);
+		quicksort(array, p + 1, hi, size);
 	}
 }
+
 /**
- * quick_sort - quick sorts an array
- * @array: array to sort
- * @size: size of array
+ * quick_sort - implementation of the quick sort algorithm
+ * @array: the array to sort
+ * @size: size of the array
+ *
+ * Return: void
  */
 void quick_sort(int *array, size_t size)
 {
 	if (array == NULL || size < 2)
 		return;
-	quickSort(array, 0, size - 1, size);
+	quicksort(array, 0, size - 1, size);
 }
