af00a375260480ed9260a5b7041480919d45b89b
Collin-5 <collins380@gmail.com>
2021-10-26 09:57:25 +0300

    quicksort time complexity

--- sort.h
+++ sort.h
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - Doubly linked list node
+ * struct listint_s - Doubly linked list node.
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
@@ -20,5 +20,6 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
